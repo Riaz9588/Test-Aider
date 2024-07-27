5912af2e21478559640f8b21a24848276a0d4e56
Riaz <mdriaz9588@gmail.com>
2024-07-27 13:59:15 +0600

    update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['dummyjson.com'],
+    domains: ['dummyjson.com', "cdn.dummyjson.com"],
   },
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import '../styles/globals.css'
 import Navigation from '../components/Navigation'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="min-h-screen bg-gray-100">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        />
+      </Head>
       <Navigation />
       <Component {...pageProps} />
     </div>
